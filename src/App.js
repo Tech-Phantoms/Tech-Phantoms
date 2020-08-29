f795bcd15d62c701b561a88a22877271a840549b
Souvik <souvikde.ns@gmail.com>
2020-08-30 00:45:55 +0530

    connected chakra-ui

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom'
 import Index from './pages/index'
 import Team from './pages/team'
 import Hacktoberfest from './pages/hacktoberfest'
+import Events from './pages/events'
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
         <Route path="/team" exact component={Team} />
 
         <Route path="/hacktoberfest" exact component={Hacktoberfest} />
+
+        <Route path="/events" exact component={Events} />
 
       </Switch>
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom'
+import { ThemeProvider, theme } from '@chakra-ui/core'
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <ThemeProvider theme={theme}>
+    <Router>
+      <App />
+    </Router>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 
