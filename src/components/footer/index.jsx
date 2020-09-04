93c025d8f62198bd950bb2c93f76e00d1a1172e5
Souvikns <souvikde.ns@gmail.com>
2020-09-05 01:07:13 +0530

    Update index.jsx

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -2,19 +2,38 @@ import React from 'react'
 import {
     Box,
     Text,
-    SimpleGrid
+    SimpleGrid,
+    Flex
 } from '@chakra-ui/core'
 
 const Footer = props => {
 
     return <>
-        <Box p={10} style={{marginTop: '120px'}}>
+        <Box p={10} style={{marginTop: '120px'}} bg="tomato" color="white">
             <SimpleGrid columns={2} spacing={4}>
                 <Box>
                     <Text>@2020 Tech_Phantoms</Text>
                 </Box>
 
-                <Box style={{alignContent: 'end'}}>icons</Box>
+                <Box style={{alignContent: 'end'}}>
+                    <Flex>
+                        <Flex flexGrow={1} />
+
+                        <Flex>
+                            <SimpleGrid columns={2} spacing={2}>
+
+                                <Box>
+                                    github
+                                </Box>
+                                <Box>
+                                    Linkedin
+                                </Box>
+
+                            </SimpleGrid>
+                        </Flex>
+
+                    </Flex>
+                </Box>
 
             </SimpleGrid>
         </Box>
