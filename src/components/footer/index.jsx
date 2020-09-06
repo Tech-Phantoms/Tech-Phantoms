0709994a32f6a58c8ac27efa0489d2b8d3561247
Abir <abir.pal899@gmail.com>
2020-09-06 16:30:02 +0530

    Updated Footer Alignments 😋

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -20,28 +20,34 @@ const Footer = props => {
         <Box p={10} style={{ marginTop: '120px', }} bg="tomato" color="white">  
         
         {/* Using SimpleGrid ( 2 Columns (Left Logo and Right Social Handles) ) */}
-            <SimpleGrid columns={2} spacing={1}>
+            <SimpleGrid columns={3} spacing={1}>
                 
                 {/* Left Logo */}
                 <Box>
                     <Link target="blank" href="#">
-                                        <img src={Logo} alt="" width="25px" /> 
+                                        <img src={Logo} alt="" width="55px" /> 
                     </Link>
 
 
                 </Box>
 
+                <Box style={{alignContent:'center'}}>
+<center>{"</>"} with<br/> 💓 <br/>by Tech Phantoms</center>
+                </Box>
+
+
+
                 {/* Right Social Handles */}
                 <Box style={{ alignContent: 'end' }}>
 
                     {/* Flex  default flex-direction:row */}
-                    <Flex>
+                    <Flex >
                         {/* Setting flexgrow level 1, assigning equal space to every icon */}
-                        <Flex flexGrow={1} />
+                        <Flex flexGrow={1}  />
 
                                     {/* 3 Column Grid for 3 different social media */}
                                     {/* Note: Adding a social media handle, requires incrementing column={previous_value+1} */}
-                                    <SimpleGrid columns={3} spacing={4}>
+                                    <SimpleGrid columns={3} spacing={4}  >
                                         
                                         {/* GitHub Link */}
                                         <Box>
@@ -66,6 +72,7 @@ const Footer = props => {
                         </Flex>
                     <Flex/>
                 </Box>
+              
             </SimpleGrid>
         </Box>
     </>
