7a1ed83ae25acd3c4d15b10d39d51c919b1b461a
Abir <abir.pal899@gmail.com>
2020-09-06 16:30:44 +0530

    Drawer Updated 😊

--- src/components/navbar/drawer.jsx
+++ src/components/navbar/drawer.jsx
@@ -8,7 +8,8 @@ import {
     DrawerBody,
     Link
 } from '@chakra-ui/core'
-
+import CloseButtton from '../../assets/sidenavbar/closeSideBar.svg'
+import Logo from '../../assets/sidenavbar/top_logo.svg'
 // Side Navigation Drawer 
 const MenuDrawer = props => {
     
@@ -29,9 +30,12 @@ const MenuDrawer = props => {
         {/* Overlaying the background  */}
         <DrawerOverlay>
         
-            <DrawerContent  background="black" color="white" width={200} letterSpacing={5} lineHeight={3} textDecoration="none" fontFamily="Segoe UI">
+            <DrawerContent  background="black" paddingTop={10} color="white" width={200} letterSpacing={5} lineHeight={3} textDecoration="none" fontFamily="Segoe UI">
                 <DrawerBody>
-                    {/* Using map function to create Links and all */}
+                  
+                    <center><Link href='/' ><img src={Logo} width="160px" alt=""/></Link><br/></center>
+                  {/* Using map function to create Links and all */}
+                    <center><Link onClick={props.close} ><img alt="" src={CloseButtton}/></Link><br/></center>
                     {paths.map(el => <><Link href={el.path}>{el.name}</Link> <br /></>)}
             
                 </DrawerBody>
