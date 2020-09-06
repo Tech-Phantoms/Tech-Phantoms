787d3ead3555de4772fb28018fda339442e559bd
Abir <abir.pal899@gmail.com>
2020-09-06 12:02:32 +0530

    Removed Redux, Code Cleaning 😊

--- src/index.js
+++ src/index.js
@@ -1,46 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ThemeProvider, theme, CSSReset } from '@chakra-ui/core'
-import "./index.css";
+// Importing Footers from footer component
 import FooterContent from './components/footer'
 
+//footer component
 const Footer = () => (
   <footer className="footer">
     <FooterContent />
   </footer>
 );
-
+//Component with App as class
 const MainApp = () => {
   return (
-
-    <ThemeProvider theme={theme} >
-      <CSSReset />
-      <Router>
         <div className="content">
           <App />
         </div>
-      </Router>
-    </ThemeProvider>
-
-  )
+ )
 }
-const store = createStore(() => [], {}, console.log('reduxDUMMYStore for Rider Provider'));
+//Wrapping Parameter
 const Wrapper = () => (
-  <Provider store={store}>
     <MainApp />
-  </Provider>
 );
+
+//function to wrap component with footer
 const withFooter = (WrappedComponent) => () => [
   <WrappedComponent key="1" />,
   <Footer key="2" />
 ];
+
+//MainApp wrapped with footer
 const WrapperWithFooter = withFooter(Wrapper);
 ReactDOM.render(
-  <WrapperWithFooter />,
+              <ThemeProvider theme={theme} >
+                <CSSReset />
+                  <Router>
+                   <WrapperWithFooter /> 
+                  </Router>
+              </ThemeProvider>,
   document.getElementById('root')
 );
 
