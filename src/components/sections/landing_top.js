5516da565afba4ea8c3d2aa5db80e5466bfe7ef3
Abir <abir.pal899@gmail.com>
2020-09-08 23:25:24 +0530

    🥺Mobile Logo View

--- src/components/sections/landing_top.js
+++ src/components/sections/landing_top.js
@@ -9,14 +9,14 @@ import { Box, Text, SimpleGrid, Image } from "@chakra-ui/core";
 const LandingTop = props => {
   return (
     <div>
-      <Box padding="10px" w="100%" mt={["100px", "200px"]} mb={["100px", "200px"]} color="#ED654B">
-        <SimpleGrid columns="2" spacing={1}>
+      <Box padding="10px" w="100%" mt={{md: "150px",xs: "5px",sm: "100px", lg: "200px"}} mb={["100px", "200px"]} color="#ED654B">
+        <SimpleGrid columns={{"lg":2,"md":2,"xs":1,"sm":1}} spacing={1}>
           <Box >
-            <center><Image src={logo} width={{lg: "40%", md:"60%", sm:"50%", xs:"75%"}} alt="lightlogo" /></center>
+            <center><Image src={logo} width={{lg: "40%", md:"60%", sm:"50%", xs:"55%"}} alt="lightlogo" py={5} /></center>
           </Box>
 
           <Box >
-            <center><Image src={LightMode_techphantoms} width={{lg: "60%", md:"60", sm:"25", xs:"90%"}} alt="LightMode_techphantoms" /></center>
+            <center><Image src={LightMode_techphantoms} width={{lg: "60%", md:"60", sm:"25", xs:"60%"}} alt="LightMode_techphantoms" /></center>
             <center><Text fontSize={["10px", "sm"]} p={[2, 4, 10]}>A Technical Community <br /> By</Text></center>
             <center><img src={LightMode_MLSA_Badge} width={{lg: "20%", md:"10%", sm:"10%", xs:"10%"}} alt="LightMode_MLSA_Badge" /></center>
 
