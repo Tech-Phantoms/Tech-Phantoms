ef30b9fc23167102af290c09b887aff29e4c42ee
Joshua Poddoku <201751033@iiitvadodara.ac.in>
2020-09-10 20:36:26 +0530

    added font files in public/fonts

--- src/pages/events.jsx
+++ src/pages/events.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import EventHolder from '../components/EventHolder'
-
+import CalendarComponent from '../components/EventHolder/CalendarComponent'
+import {
+    Box,
+    Flex
+} from '@chakra-ui/core'
 
 const Events = props => {
 
-    return <>
-        <span style={{ fontSize: 50 }}>Events</span><br />
-        {/* Sample Event Holder */}
-        <EventHolder
+
+    return (
+        <div >
+        <Box className style={{ fontSize: 50 }}>Events</Box><br />
+        <Box pos="fixed" w="100%" zIndex={2}>
+        <CalendarComponent/>
+        </Box>
+        <Flex align="center" justify="center">
+         <EventHolder
          eventDesc="Git and GitHub 101"
          eventDate="23/08/2020"
          eventTitle="HacktoberFest with Tech Phantoms"
@@ -15,8 +24,10 @@ const Events = props => {
          mlsaEvent={true} 
          eventRec="sadsd"
          />
-
-    </>
+         
+        </Flex>
+         </div>
+    )
 }
 
 export default Events
