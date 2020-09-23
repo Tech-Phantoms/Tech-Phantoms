9964f11625a2c49d1b814c07924eecdfe4a45bf4
Souvik <souvikde.ns@gmail.com>
2020-09-23 18:46:17 +0530

    fixing #67

--- src/components/EventHolder/events.jsx
+++ src/components/EventHolder/events.jsx
@@ -3,26 +3,39 @@ import { events } from '../../data/airtable'
 import Holder from './holder'
 import {
     Box,
-   
-  } from '@chakra-ui/core'
+
+} from '@chakra-ui/core'
 const Events = props => {
     let [evnts] = events.useEvents()
 
     return <>
         <Box>
-        {evnts.map(el => {
-            console.log(el)
+            {evnts.map(el => {
+                console.log(el)
 
-            return <>
-                <Holder
-                    title={el.fields.title}
-                    mlsaEvent={el.fields.mlsa_event}
-                    eventImg={el.fields.eventImg[0].url}
-                    description={el.fields.description}
-                    date={el.fields.date}
-                /><br/>
-            </>
-        })}
+                if (el.fields.eventImage) {
+                    return <>
+                        <Holder
+                            title={el.fields.title}
+                            mlsaEvent={el.fields.mlsa_event}
+                            eventImg={el.fields.eventImg[0].url || ''}
+                            description={el.fields.description}
+                            date={el.fields.date}
+                        /><br />
+                    </>
+                } else {
+                    return <>
+                        <Holder
+                            title={el.fields.title}
+                            mlsaEvent={el.fields.mlsa_event}
+                            description={el.fields.description}
+                            date={el.fields.date}
+                        /><br />
+                    </>
+                }
+
+
+            })}
         </Box>
     </>
 }
