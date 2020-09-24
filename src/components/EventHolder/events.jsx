d7bf1a9cbfae65f9b947c39fd008f833642f2caa
Souvik <souvikde.ns@gmail.com>
2020-09-24 21:09:17 +0530

    fixed time and card design

--- src/components/EventHolder/events.jsx
+++ src/components/EventHolder/events.jsx
@@ -9,26 +9,28 @@ const Events = props => {
     let [evnts] = events.useEvents()
 
     return <>
-        <Box>
+        <Box style={{margin: '2.3rem'}}>
             {evnts.map(el => {
 
                 if (el.fields.eventImg) {
                     return <>
                         <Holder
                             title={el.fields.title}
-                            mlsaEvent={el.fields.mlsa_event}
+                            mlsa_event={el.fields.mlsa_event}
                             eventImg={el.fields.eventImg[0].url || ''}
-                            description={el.fields.description}
+                            description={el.fields.desription}
                             date={el.fields.date}
-                        /><br />
+                            reg_link={el.fields.reg_link}
+                        />
                     </>
                 } else {
                     return <>
                         <Holder
                             title={el.fields.title}
-                            mlsaEvent={el.fields.mlsa_event}
-                            description={el.fields.description}
+                            mlsa_event={el.fields.mlsa_event}
+                            description={el.fields.desription}
                             date={el.fields.date}
+                            reg_link={el.fields.reg_link}
                         /><br />
                     </>
                 }
