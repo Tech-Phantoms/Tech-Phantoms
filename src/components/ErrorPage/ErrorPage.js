a72e454b69068fd4372e7449101d43fa0017c1fc
elitechef <srmandmishra@gmail.com>
2020-10-01 14:12:25 +0530

    add 404 error page

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -13,7 +13,10 @@ export default function ErrorPage() {
 
             <div className="msg">
                 <p>Uh oh!</p>
+
                 <p id="medium">Looks Like You got lost.</p>
+
+                
                 <p id="small">
                     The page you are looking for does not exist. How you got
                     here is a mystery. Please go back to the homepage.
