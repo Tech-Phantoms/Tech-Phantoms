e1bb9ef02becc19ec22ae4054f3169d486e402d3
Sandeep Kumar <sandeepkumar070200@gmail.com>
2020-10-06 05:36:08 +0000

    Converted 404 page to material ui

--- src/components/PageNotFound/index.jsx
+++ src/components/PageNotFound/index.jsx
@@ -1,49 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import {
-    PseudoBox,
-    Image,
-    Flex,
-    Heading,
-    Box,
-} from "@chakra-ui/core";
-import emotion from '../../assets/404sad_face.svg'
+import { Typography,Button,Box } from '@material-ui/core';
+import emoticon from '../../assets/404sad_face.svg'
+
+const btnstyle = {
+      color: "white",
+      backgroundColor: "tomato",
+      boxShadow: "none",
+};
 
 const NotFoundPage = () => (
-    <Box display={Flex} 
-    alignItems="center" 
-    bg="tomato" 
-    flexDirection="column" 
-    justifyContent="Center" 
-    w="100%" 
-    height="315px" 
-    color="white">
-        <Image src={emotion} maxW="90px" alt="Page not Found"/>
-        
-        <Heading as="h1" size="2xl">
-            404!
-        </Heading>
-
-        <Heading as="h3" size="lg" textAlign="center">
-            Looks like you have travelled way too far...
-        </Heading>
-
+    <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" height="calc(100vh - 310.7px)">
+        <img src={emoticon} alt="...." width="100px"/>
+        <Typography variant="h4" align="center">
+            Oh no! Page Not Found.
+        </Typography>
+        <Typography variant="subtitle1" align="center">
+        Looks like you have travelled way too far...
+        </Typography>
         <Link to="/">
-            <PseudoBox 
-            as="button" 
-            bg="white" 
-            py={2} 
-            px={4} 
-            ml={3} 
-            my={5} 
-            rounded="md" 
-            fontWeight="semibold" 
-            color="tomato" 
-            _focus={{ boxShadow: "outline" }}>
+            <Button style={btnstyle}>
                 Go back to home
-            </PseudoBox>
+            </Button>
         </Link>
-
     </Box>
 );
 export default NotFoundPage;
