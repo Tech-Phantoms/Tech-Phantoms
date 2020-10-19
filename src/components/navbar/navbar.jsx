5bc7d390812474d276df07a01291a6bd73b41521
Abir <abir.pal899@gmail.com>
2020-10-19 11:45:44 +0530

    Removed Unused Vars 😊

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
