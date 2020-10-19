d38e009d00aa8d90b40c9ac56edd38f5efada6e1
Abir <abir.pal899@gmail.com>
2020-10-19 11:45:10 +0530

    Removed Unused Vars 😊

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,29 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
-
-
 
 
 const Navbar = () => {
-    const classes = useStyles();
-
     return(
         <Toolbar>
             <Button color="inherit">About Us</Button>
