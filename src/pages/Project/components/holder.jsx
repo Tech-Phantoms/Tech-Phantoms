32740ebb9f3b7d21a78e4f9edac74e3df24d85a4
Souvik <souvikde.ns@gmail.com>
2020-10-19 11:29:01 +0530

    Update holder.jsx

--- src/pages/Project/components/holder.jsx
+++ src/pages/Project/components/holder.jsx
@@ -38,32 +38,32 @@ const Holder = props => {
 
     return (
         <div>
-            <a target="blank" href={props.issueUrl}>
+            <a target="blank" href={issueUrl}>
                 <Paper variant="outlined" className={classes.paper}>
                     <Grid container spacing={0}>
                         <Grid item xs={12}>
                             <Typography variant="h5">
-                                {props.repo_name}
+                                {repo_name}
                             </Typography>
                         </Grid>
                         <Grid item xs={12}>
                             <Typography variant="caption">
-                                {props.description}
+                                {description}
                             </Typography>
                         </Grid>
                     </Grid>
                     <div className={classes.divider} />
                     <Grid container spacing={4}>
                         <Grid item xs={6}>
-                            <Languagebadge language={props.language} />
+                            <Languagebadge language={language} />
                         </Grid>
                         <Grid item xs={3}>
-                            <Badge badgeContent={props.issues}>
+                            <Badge badgeContent={issues}>
                                 <Info color="primary" fontSize="small" />
                             </Badge>
                         </Grid>
                         <Grid item xs={3}>
-                            <Badge badgeContent={props.stars}>
+                            <Badge badgeContent={stars}>
                                 <Star color="secondary" fontSize="small" />
                             </Badge>
                         </Grid>
