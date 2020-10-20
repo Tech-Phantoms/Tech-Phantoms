8620f4f6d3472cf5dbd73b331f4018a9269898ba
Abir <abir.pal899@gmail.com>
2020-10-21 01:46:32 +0530

    Update index.jsx

--- src/pages/team/sections/coremembers/index.jsx
+++ src/pages/team/sections/coremembers/index.jsx
@@ -1,39 +1,41 @@
 import React from "react";
-import Grid from '@material-ui/core/Grid';
+import { Container, Grid, Paper } from '@material-ui/core/';
 import PropTypes from 'prop-types';
 import Holder from "../../components/holder/holder";
 
 const DisplayTeams = (props) => {
 
     return (
-        <center>
-        <Grid container >
-        
+
+        <Grid container>
+
             {
                 props.members.map((member) => (
-                    <Grid spacing={5}>
-                    <Grid container item md={3} xs={12} lg={3} sm={12}>
-                    <Holder id={member.id}
-                        name={member.get("name")}
-                        imageUrl={member.get('avatar_url')}
-                        githubUrl={member.get('github_profile')}
-                        linkedinUrl={member.get('linkedin_profile')}
-                        gender={member.get('gender')}
-                        instagramUrl={member.get('instagram_profile')}
-                        mediumUrl={member.get('medium_profile')}
-                        position={member.get('position')}
-                        /></Grid></Grid>
+                    <Grid item xs={12} sm={12} md={4} lg={4}>
+                        <Holder id={member.id}
+                            name={member.get("name")}
+                            imageUrl={member.get('avatar_url')}
+                            githubUrl={member.get('github_profile')}
+                            linkedinUrl={member.get('linkedin_profile')}
+                            gender={member.get('gender')}
+                            instagramUrl={member.get('instagram_profile')}
+                            mediumUrl={member.get('medium_profile')}
+                            position={member.get('position')}
+                        />
+                    </Grid>
                 ))
             }
-        </Grid></center>
+
+        </Grid>
+
 
     )
 
 
 }
 
-DisplayTeams.propTypes={
-        members:PropTypes.array
+DisplayTeams.propTypes = {
+    members: PropTypes.array
 }
 
 
