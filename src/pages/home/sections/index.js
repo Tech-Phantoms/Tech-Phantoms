e900c6d5c53a22653af55adfb66d634490efe411
SuNiL-Chau <1234huts@gmail.com>
2020-12-29 11:44:43 +0530

    clean directory & added needed directories with Event page design

--- src/pages/home/sections/index.js
+++
@@ -1,6 +0,0 @@
-import LandingTop from './landing_top';
-import LandingMiddle from './landing_middle'
-import LandingBottom from './landing_bottom'
-import Social from './social'
-
-export { LandingTop, LandingMiddle, LandingBottom, Social }
