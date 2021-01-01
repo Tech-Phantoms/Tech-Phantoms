81dc0c59d8db1ec08ac367dbf9b67cf081cc997b
Souvik <souvikde.ns@gmail.com>
2021-01-01 17:35:59 +0530

    Update index.jsx

--- src/pages/event/index.jsx
+++ src/pages/event/index.jsx
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Events from './sections/events'
 import { Hidden } from "@material-ui/core/";
 import DevTag from '../../components/underDevTag/'
 import {
     Container
 } from '@material-ui/core'
+import { events } from '../../data/airtable';
 
 const Index = props => {
     const Navbar = props.navbar
+
+    let [eventList, setEventList] = useState([]);
+    let [datesList, setDateList] = useState([]);
+    useEffect(() => {
+        let [evnts, dates] = events.useEvents();
+        setEventList(evnts);
+        setDateList(dates);
+    }, []);
+
     return (
         <div>
             <Hidden smDown>
                 <Navbar />
             </Hidden>
-            <DevTag/>
+            <DevTag />
             <center>
                 <span style={{ fontSize: 50 }}>Events</span><br />
             </center>
