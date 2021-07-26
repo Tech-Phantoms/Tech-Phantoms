2056bf9f5b4b1bc2220c8d499cd5781f3edad9b7
Abir <abir.pal899@gmail.com>
2021-07-26 22:40:40 +0530

    Update SpecialBanner.jsx

--- src/pages/LandingPage/components/SpecialBanner.jsx
+++ src/pages/LandingPage/components/SpecialBanner.jsx
@@ -4,21 +4,23 @@ import ROUTES from "../../../routes";
 
 function SpecialBanner() {
   return (
-    <Center>
-    <Link href={ROUTES.socials.apihacks} isExternal>
-      <Box
+     <Center>
+       <Box
         bgGradient="linear(93.96deg, #ff3D00 -19.25%,#FFE600 72.85%,#FC2FCF 144.15%)"
         h="fit-content"
         w="85%"
         p="2"
         rounded="5"
       >
+    <Link href="https://apihacks.co" isExternal>
+     
         <Text color="black" fontWeight="semibold">
           Seasonal Hackathon
         </Text>
         <Text color="black">apihacks.co</Text>
-      </Box>
+     
      </Link>
+     </Box>
     </Center>
   );
 }
