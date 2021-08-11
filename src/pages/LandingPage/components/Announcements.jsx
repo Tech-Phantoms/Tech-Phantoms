0c8669327372717e8a1a39fb86f40e56061e360b
Calvin Dsouza <37261038+kalzen15@users.noreply.github.com>
2021-08-12 02:40:19 +0530

    feat: Announcements linked with deta

--- src/pages/LandingPage/components/Announcements.jsx
+++ src/pages/LandingPage/components/Announcements.jsx
@@ -1,7 +1,19 @@
-import React from "react";
-import { Box, Flex, Heading } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import { Box, Flex, Heading, Link, Text } from "@chakra-ui/react";
 import colors from "../../../colors";
 function Announcements() {
+  const [ann, setAnn] = useState([]);
+  useEffect(() => {
+    fetchAnnouncements();
+  }, []);
+
+  const fetchAnnouncements = async () => {
+    fetch(process.env.REACT_APP_API_URL, { method: "GET" })
+      .then((res) => res.json())
+      .then((response) => setAnn(response))
+      .catch((err) => console.log(err));
+  };
+
   return (
     <Box
       borderWidth="1px"
@@ -11,12 +23,23 @@ function Announcements() {
       ml={6}
       mt={6}
       w="70%"
+      minW="20vw"
+      maxW="27vw"
     >
       <Flex>
         <Heading m={4} as="h4" size="md" color={colors.headings}>
           Announcements
         </Heading>
       </Flex>
+      {ann.map((val) => {
+        return (
+          <Box m={2} align="center">
+            <Link href={val.link} target="_blank">
+              <Text isTruncated>{val.title}</Text>
+            </Link>
+          </Box>
+        );
+      })}
     </Box>
   );
 }
