fbfd2e3159d8f1f666f71850447a39b8ac69ec0b
Aniket Khajanchi <ani.khajanchi257@gmail.com>
2021-12-11 12:25:35 +0530

    test

--- .next/static/i9LWVWyc17n-JnLt0BwqL/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-277ff2557ca34a90.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
