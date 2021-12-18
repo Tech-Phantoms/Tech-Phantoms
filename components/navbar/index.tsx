79d24879ff340ecd1e3fdf6591253e40d158b108
Abir <abir.p@affinidi.com>
2021-12-18 20:40:07 +0530

    Add Navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,9 +1,58 @@
-const NavItems  =[{}]
-const Navbar = (props:any) =>{
+// import Image from 'next/image'
+
+const NavItems = [
+    {
+        text: "Maintainers Program"
+    },
+    {
+        text: "Team"
+    },
+    {
+        text: "Showcase"
+    },
+]
+
+const Navbar = (props: any) => {
     return <>
-    <div className="text-3xl p-14 bg-black-200">
-ss
-    </div>
+        <div className="text-3xl  p-4 bg-stone-900	 flex">
+            <div className="grow">
+                <p className="font-bold text-white">
+                    Tech Phantoms
+                </p>
+            </div>
+            {
+                NavItems && NavItems.map((item) => {
+                    return <>
+                        <div>
+                            <p className="ml-4 p-2 mr-4 text-sm text-white 
+                            hover:bg-orange-300
+                            hover:rounded 
+                            hover:text-black
+                            hover:cursor-pointer">
+                                {item.text}
+                            </p>
+                        </div>
+                    </>
+                })
+            }
+            <div>
+                <p className=" bg-indigo-500 rounded ml-4 p-2 mr-4 text-sm text-white
+                hover:bg-indigo-600
+                hover:cursor-pointer
+                ">
+                    Join Discord
+                </p>
+            </div>
+            <div>
+                <p className="bg-lime-600 rounded ml-4 p-2 mr-4 text-sm text-white
+                hover:bg-lime-700	
+                hover:cursor-pointer
+                ">
+                    Join Github
+                </p>
+            </div>
+
+        </div>
     </>
 }
 export default Navbar;
